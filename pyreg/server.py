edcb3610d64826136b48e213662ca993a725ac72
Andrew Miller <amiller@dappervision.com>
2011-05-21 01:42:49 -0400

    Server accepts optional ipy_options argument
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- pyreg/server.py
+++ pyreg/server.py
@@ -5,44 +5,47 @@ import logging
 import threading
 import traceback
 
-import IPython
 from IPython.Shell import IPShellEmbed
-def start():
-	
-	ipshell = IPShellEmbed(sys.argv[1:], user_ns=scope)
-	#ipshell.IP.runlines('from pylab import ion; ion()')
-	ipshell()
-	
-	# Put this here to help pyglet clean up apps immediately
-	# Pointless if no one is using pyglet, oh well
-	try: 
-		import pyglet
-		pyglet.app.exit() 
-	except: pass
-	sys.exit()
-	
-def main():
-	global scope
-	scope = dict()
 
-	# TODO: Parse command line options, find 'port'
-	port = 21000
-	browser.setup(scope, port)
-	#print("Ajax Server started...")
-	logging.disable(logging.WARNING)
-	
-	thread = threading.Thread(target=start)
-	thread.start()
-	
-	try:
-		for f in sys.argv[1:]:
-			execfile(f, scope)
-	except Exception, e:
-		traceback.print_exc()
-		
-	browser.start()
-	thread.join()
-	
-if __name__ == "__main__":	
-	main()
+scope = {}
 
+
+def start(ipy_options={}):
+    ipshell = IPShellEmbed(sys.argv[1:], user_ns=scope, **ipy_options)
+    #ipshell.IP.runlines('from pylab import ion; ion()')
+    ipshell()
+
+    # Put this here to help pyglet clean up apps immediately
+    # Pointless if no one is using pyglet, oh well
+    try:
+        import pyglet
+        pyglet.app.exit()
+    except:
+        pass
+    sys.exit()
+
+
+def main(**kwargs):
+    global scope
+
+    # TODO: Parse command line options, find 'port'
+    port = 21000
+    browser.setup(scope, port)
+    #print("Ajax Server started...")
+    logging.disable(logging.WARNING)
+
+    thread = threading.Thread(target=start,kwargs=kwargs)
+    thread.start()
+
+    try:
+        for f in sys.argv[1:]:
+            execfile(f, scope)
+    except Exception:
+        traceback.print_exc()
+
+    browser.start()
+    thread.join()
+
+
+if __name__ == "__main__":
+    main()
